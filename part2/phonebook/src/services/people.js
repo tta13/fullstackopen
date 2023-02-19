487baee52b049470498d05803f54857f4f33cd74
Tales Alves <54077178+tta13@users.noreply.github.com>
2023-02-19 11:28:05 -0300

    Add solution for exercise 3.9

--- part2/phonebook/src/services/people.js
+++ part2/phonebook/src/services/people.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:3001/persons';
+const baseUrl = 'http://localhost:3001/api/persons';
 
 function getContact(id=undefined) {
   const request = id === undefined ? axios.get(baseUrl) : axios.get(`${baseUrl}/${id}`);
