7d8bd01a43a97e9fc79f319ff3c095aab75446e5
Tales Alves <54077178+tta13@users.noreply.github.com>
2023-02-19 11:59:32 -0300

    Add solution for ex. 3.11: make sure the frontend still works locally

--- part2/phonebook/src/services/people.js
+++ part2/phonebook/src/services/people.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:3001/api/persons';
+const baseUrl = '/api/persons';
 
 function getContact(id=undefined) {
   const request = id === undefined ? axios.get(baseUrl) : axios.get(`${baseUrl}/${id}`);
