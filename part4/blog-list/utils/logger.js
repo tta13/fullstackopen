0f1ab91305b824c882dbb82c908a0e9b83c8c01f
Tales Alves <54077178+tta13@users.noreply.github.com>
2023-03-01 10:26:22 -0300

    Add solution to exercise 4.8

--- part4/blog-list/utils/logger.js
+++ part4/blog-list/utils/logger.js
@@ -1,8 +1,14 @@
+require('dotenv').config()
+
 const info = (...params) => {
+  if(process.env.NODE_ENV === 'test') return
+
   console.log('[INFO]', ...params)
 }
 
 const error = (...params) => {
+  if(process.env.NODE_ENV === 'test') return
+  
   console.error('[ERROR]', ...params)
 }
 
